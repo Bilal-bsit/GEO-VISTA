e0b752a565ef0e012cae6a19ce83c7e1a2665362
Bilal-bsit <bsitf21e68414@gcbskp.edu.pk>
2025-07-24 12:58:41 +0500

    upload directories
    added js, inc, images folders

---
+++ inc/footer.php
@@ -0,0 +1,54 @@
+
+ 
+            <div class="tiny-footer ">
+                <div class="container ">
+                    <div class="row ">
+                        <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12 text-center  ">
+                            <p><a class="text-white" target="_blank" href="index.php">Developed By Ali Hassan & Muhammad Bilal.</a></p>
+                        </div>
+                    </div>
+                    <!-- /. tiny-footer -->
+                </div>
+            </div>
+        </div>
+        <!-- /.footer -->
+        <!-- Modal -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-body">
+                        <form>
+                            <div class="search-form">
+                                <input type="text" class="form-control" placeholder="Find here">
+                                <button type="button" class="btn btn-primary btn-sm" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">close</span>
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /.Modal -->
+   
+
+
+ <!-- /.Modal -->
+        <a href="javascript:" id="return-to-top"><i class="fa fa-angle-up"></i></a>
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="js/jquery.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/menumaker.js"></script>
+        <script src="js/jquery.sticky.js"></script>
+        <script src="js/sticky-header.js"></script>
+        <script src="js/owl.carousel.min.js"></script>
+        <script src="js/multiple-carousel.js"></script>
+        <script src="js/jquery-ui.js"></script>
+        <script src="js/date.js"></script>
+        <script src="js/return-to-top.js"></script>
+    </div>
+</body>
+
+
+</html>
---
+++ inc/head.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <title>Geo-vista</title>
+ 
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:300,300i,400,400i,700,700i,800,800i" rel="stylesheet">
+    <link href="css/owl.carousel.css" rel="stylesheet">
+    <link href="css/owl.theme.default.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/jquery-ui.css">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    
+</head>
+
+<body>
+
